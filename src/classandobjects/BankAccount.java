4aaa2c8329008e39a8405a8624329b7015788e64
ahsbth <asraj48342@gmail.com>
2024-04-21 14:11:56 +0530

    initial

---
+++ src/classandobjects/BankAccount.java
@@ -0,0 +1,16 @@
+package classandobjects;
+
+public class BankAccount {
+
+	public static void main(String[] args) {
+		First f1=new First();
+		f1.ac_balance=786896;
+		f1.acc_name="Ahsan Ansari";
+		f1.account_type="Saving Account";
+		f1.account_no="1919200100004953";
+		f1.show_acount();
+		f1.show_Balance();
+
+	}
+
+}
---
+++ src/classandobjects/First.java
@@ -0,0 +1,19 @@
+package classandobjects;
+
+ class First {
+    String acc_name;
+	protected String account_no;
+	long ac_balance;
+	String account_type;
+	
+	public void show_acount() {
+		System.out.println("Account Holder=:"+acc_name);
+		System.out.println("Account No=:"+account_no);
+		System.out.println("Account Balance=:"+ac_balance);
+		System.out.println("Account Type=:"+account_type);
+	}
+	public void show_Balance() {
+		System.out.println("Accoutn Balance is:"+ac_balance);
+	}
+
+}
